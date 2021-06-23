56db31e703ab80774dfd75b95f175b2436239292
Himanshu Kumar <himanshulohani559536@gmail.com>
2021-06-23 17:57:59 +0530

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const input = document.querySelector('input')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const search = input.value;
-    fetch('http://localhost:3000/weather?address=' + search).then((response) => {
+    fetch('/weather?address=' + search).then((response) => {
         response.json().then(data => {
             if (data.error) console.log(data.error)
             else console.log(data)
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 const app = express()
-
+const port = process.env.PORT || 3000;
 //Define path for express config
 const publicDirectory = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views') //change views to template,then we have to add this line and next one
@@ -70,6 +70,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('server is running at port 3000')
+app.listen(port, () => {
+    console.log('server is running at port ' + port)
 })
