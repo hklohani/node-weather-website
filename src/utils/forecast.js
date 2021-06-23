ba00fff1ccd1923b63efebc65dc13657b4a561c2
Himanshu Kumar <himanshulohani559536@gmail.com>
2021-06-23 18:51:04 +0530

    Added humidity to our application

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,12 @@
 const request = require('request')
 
-const forecast = (latitude,longitude,callback)=>{
-    const url = 'http://api.weatherapi.com/v1/current.json?key=cb864547bc664295871151715212006&q='+latitude+','+longitude +'&aqi=no';
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherapi.com/v1/current.json?key=cb864547bc664295871151715212006&q=' + latitude + ',' + longitude + '&aqi=no';
 
-    request({url,json:true},(error,{ body })=>{
-        if(error) callback('unable to connect with internet...',undefined);
-        else if(body.error) callback('Unable to find location',undefined)
-        else callback(undefined,`${body.current.condition.text}. it's currently ${body.current.temp_c} degrees but feels like ${body.current.feelslike_c} degrees out.`)
+    request({ url, json: true }, (error, { body }) => {
+        if (error) callback('unable to connect with internet...', undefined);
+        else if (body.error) callback('Unable to find location', undefined)
+        else callback(undefined, `${body.current.condition.text}. it's currently ${body.current.temp_c} degrees but feels like ${body.current.feelslike_c} degrees out and Humidity is ${body.current.humidity} `)
     })
 }
 
